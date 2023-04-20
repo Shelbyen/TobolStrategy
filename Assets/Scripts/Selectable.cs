b2fb817f8ef7e00f5faf5ab86c25332dff7d5db1
KirillKom <kirill-e-komarov@mail.ru>
2023-04-20 18:25:25 +0500

    UI

---
+++ Assets/Scripts/Selectable.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Selectable : MonoBehaviour
+{
+    public string Name;
+    public bool Selected;
+
+    private Building Building;
+    private Human Human;
+
+    public GameObject StatusWindow;
+    public Vector3 StatusWindowPosition;
+
+    public void Awake()
+    {
+        Building = GetComponent<Building>();
+        Human = GetComponent<Human>();
+        if (StatusWindow != null)
+        {
+            StatusWindow.transform.SetParent(GameObject.Find("Canvas").transform);
+            StatusWindow.SetActive(false);
+        }
+    }
+
+    public void Update()
+    {
+        if (StatusWindow != null) StatusWindow.transform.position = transform.position + StatusWindowPosition;
+    }
+
+    public void SelectThis()
+    {
+        Selected = true;
+        if (StatusWindow != null) StatusWindow.SetActive(true);
+    }
+
+    public void DeselectThis()
+    {
+        Selected = false;
+        if (StatusWindow != null) StatusWindow.SetActive(false);
+    }
+}
